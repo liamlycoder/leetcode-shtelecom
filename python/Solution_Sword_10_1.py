5b88e06b288e09f33ff33cddcf9ef5767664f406
liamlycoder <luyu.real@qq.com>
2020-09-29 16:50:45 +0800

    剑指offer的第10-1题Python解法

---
+++ python/Solution_Sword_10_1.py
@@ -0,0 +1,13 @@
+from functools import lru_cache
+
+
+class Solution:
+    @lru_cache(None)
+    def fib(self, n):
+        if n < 2:
+            return n
+        return (self.fib(n-1) + self.fib(n-2)) % 1000000007
+
+
+if __name__ == '__main__':
+    print(Solution().fib(8))
