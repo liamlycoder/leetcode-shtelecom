ffaedc539dd331f425c07fd011842d989d9efab6
liamlycoder <luyu.real@qq.com>
2020-09-30 16:48:11 +0800

    提交剑指offer第15题的java,python题解

---
+++ java/src/Solution_Sword_15.java
@@ -0,0 +1,10 @@
+public class Solution_Sword_15 {
+    public int hammingWeight(int n) {
+        int res = 0;
+        while (n != 0) {
+            ++res;
+            n = (n & (n - 1));
+        }
+        return res;
+    }
+}
