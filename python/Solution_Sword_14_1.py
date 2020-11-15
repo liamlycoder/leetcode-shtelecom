dbfc72f93c72da17a696ba0d6c0c3dedb120c025
liamcoder <luyu.real@qq.com>
2020-11-15 11:55:58 +0800

    modify 14-1

---
+++ python/Solution_Sword_14_1.py
@@ -0,0 +1,11 @@
+# !/usr/bin/python
+# coding: utf8
+# Time: 2020/10/18 10:50
+# Author: Liam
+# E-mail: luyu.real@qq.com
+# Software: PyCharm
+class Solution:
+    def cuttingRope(self, n: int) -> int:
+        
+
+
