781d5f4d36e5e72fd1d50d9c8e15420b8cd05d73
liamlycoder <luyu.real@qq.com>
2020-12-01 08:44:48 +0800

    提交leetcode第680题解“

---
+++ java/src/Solution680.java
@@ -0,0 +1,26 @@
+/**
+ * @author liamcoder
+ * @date 2020/12/1 8:37
+ */
+public class Solution680 {
+    public boolean validPalindrome(String s) {
+        for (int i = 0, j = s.length() - 1; i < j; ++i, --j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return isPalindrome(s, i, j-1) || isPalindrome(s, i+1, j);
+            }
+        }
+        return true;
+    }
+
+    private boolean isPalindrome(String s, int i, int j) {
+        while (i < j) {
+            if (s.charAt(i) == s.charAt(j)) {
+                ++i;
+                --j;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
