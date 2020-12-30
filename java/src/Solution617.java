74e644b6f333c9b52aa9832e70e1d26919ac3402
liamlycoder <luyu.real@qq.com>
2020-12-30 15:43:24 +0800

    leetcode617

---
+++ java/src/Solution617.java
@@ -0,0 +1,20 @@
+import entity.TreeNode;
+
+/**
+ * @author liamcoder
+ * @date 2020/12/30 15:35
+ */
+public class Solution617 {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) {
+            return null;
+        }
+        if (t1 == null || t2 == null) {
+            return t1 == null ? t2 : t1;
+        }
+        t1.val += t2.val;
+        t1.left = mergeTrees(t1.left, t2.left);
+        t1.right = mergeTrees(t1.right, t2.right);
+        return t1;
+    }
+}
