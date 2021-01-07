9cf4318333a449c12b354ab7c9e11309c468699a
liamcoder <luyu.real@qq.com>
2021-01-07 20:22:53 +0800

    leetcode每日一题

--- java/src/Solution547.java
+++ java/src/Solution547.java
@@ -1,6 +1,26 @@
-package PACKAGE_NAME;/**
- * @author  liamcoder
- * @date  2021/1/7 8:21 下午
+/**
+ * @author liamcoder
+ * @date 2021/1/7 8:21 下午
  */
-    public class Solution547 {
+class Solution547 {
+    public int findCircleNum(int[][] M) {
+        boolean[] visited = new boolean[M.length];
+        int ret = 0;
+        for(int i = 0; i < M.length; ++i) {
+            if(!visited[i]) {
+                dfs(M, visited, i);
+                ret++;
+            }
+        }
+        return ret;
+    }
+
+    private void dfs(int[][] m, boolean[] visited, int i) {
+        for(int j = 0; j < m.length; ++j) {
+            if(m[i][j] == 1 && !visited[j]) {
+                visited[j] = true;
+                dfs(m, visited, j);
+            }
+        }
+    }
 }
