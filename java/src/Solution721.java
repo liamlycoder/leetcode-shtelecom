3d9b1d27b1bb618e117e1c197ede8cd49e17e583
luyu <luy28@chinatelecom.cn>
2021-01-18 09:01:58 +0800

    每日一题

---
+++ java/src/Solution721.java
@@ -0,0 +1,54 @@
+import entity.DSU;
+
+import java.util.*;
+
+/**
+ * @author liamcoder
+ * @date 2021/1/18 8:43
+ */
+public class Solution721 {
+    public List<List<String>> accountsMerge(List<List<String>> accounts) {
+        Map<String, Integer> emailToIndex = new HashMap<String, Integer>();
+        Map<String, String> emailToName = new HashMap<String, String>();
+        int emailsCount = 0;
+        for (List<String> account : accounts) {
+            String name = account.get(0);
+            int size = account.size();
+            for (int i = 1; i < size; i++) {
+                String email = account.get(i);
+                if (!emailToIndex.containsKey(email)) {
+                    emailToIndex.put(email, emailsCount++);
+                    emailToName.put(email, name);
+                }
+            }
+        }
+        DSU dsu = new DSU(emailsCount);
+        for (List<String> account : accounts) {
+            String firstEmail = account.get(1);
+            int firstIndex = emailToIndex.get(firstEmail);
+            int size = account.size();
+            for (int i = 2; i < size; i++) {
+                String nextEmail = account.get(i);
+                int nextIndex = emailToIndex.get(nextEmail);
+                dsu.union(firstIndex, nextIndex);
+            }
+        }
+        Map<Integer, List<String>> indexToEmails = new HashMap<Integer, List<String>>();
+        for (String email : emailToIndex.keySet()) {
+            int index = dsu.find(emailToIndex.get(email));
+            List<String> account = indexToEmails.getOrDefault(index, new ArrayList<String>());
+            account.add(email);
+            indexToEmails.put(index, account);
+        }
+        List<List<String>> merged = new ArrayList<List<String>>();
+        for (List<String> emails : indexToEmails.values()) {
+            Collections.sort(emails);
+            String name = emailToName.get(emails.get(0));
+            List<String> account = new ArrayList<String>();
+            account.add(name);
+            account.addAll(emails);
+            merged.add(account);
+        }
+        return merged;
+    }
+}
