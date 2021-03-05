30edf432e954f30411dc739fc959169c5a9c4128
luyu <luyu.real@qq.com>
2021-03-05 16:05:16 +0800

    add go solution for problem 003

---
+++ golang/go-solution003.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+func lengthOfLongestSubstring(s string) int {
+	hashTable := map[byte]int{}
+	result, left := 0, 0
+	for index, ch := range s {
+		if repeat, ok := hashTable[byte(ch)]; ok {
+			left = max(left, repeat + 1)
+		}
+		hashTable[byte(ch)] = index
+		result = max(result, index - left + 1)
+	}
+	return result
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	} else {
+		return y
+	}
+}
+
+func main() {
+	s := "abcdefg"
+	for i, j := range s {
+		fmt.Println("i = ", i, "j = ", string(j))
+	}
+
+}
