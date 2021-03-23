6a3368735ac8efe822ed99bee963b51325a9c30d
luyu <luyu.real@qq.com>
2021-03-23 08:56:41 +0800

    今日第一题

--- golang/go-solution581.go
+++ golang/go-solution581.go
@@ -1,27 +1,28 @@
 package main
 
+// 最短无序连续子数组
 func findUnsortedSubarray(nums []int) int {
-	mxv := nums[0]
-	miv := nums[len(nums) - 1]
-	j := len(nums) - 1
-	l, r := 0, 0
+	n := len(nums)
+	stack := make([]int, n)
+	l, r := n, 0
 	for i, v := range nums {
-		if v < mxv {
-			l = i
-		} else {
-			mxv = v
+		for len(stack) != 0 && nums[stack[len(stack) - 1]] > v {
+			l = min(l, stack[len(stack) - 1])
+			stack = stack[:len(stack) - 1]
 		}
-
-		if v > miv {
-			r = j
-		} else {
-			miv = nums[j]
-		}
-		j--
+		stack = append(stack, i)
 	}
-
-	if l >= r {
+	stack = make([]int, n)
+	for i := n - 1; i >= 0; i-- {
+		for len(stack) != 0 && nums[stack[len(stack) - 1]] < nums[i] {
+			r = max(r, stack[len(stack) - 1])
+			stack = stack[:len(stack) - 1]
+		}
+		stack = append(stack, i)
+	}
+	if r - l > 0 {
+		return r - l + 1
+	} else {
 		return 0
 	}
-	return r - l + 1
 }
